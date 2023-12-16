c7969a836c959be03b22d9b581ef7df5c39fa89a
nqbao1501 <127926528+nqbao1501@users.noreply.github.com>
2023-12-17 00:46:25 +0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -23,7 +23,7 @@ let timerStatus = "stopped";
 
 function playAlarm()
 {
-    let alarm = new Audio("https://github.com/nqbao1501/pomodoro/blob/main/radar.mp3")
+    let alarm = new Audio("radar.mp3")
     alarm.play();
 }
 //timer function
